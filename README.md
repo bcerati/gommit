2483d6c1497bdd2ea8aaee90316630d08d948b84
Boris CERATI <contact@boris-cerati.fr>
2020-02-28 10:30:19 +0100

    [PACKAGE] feature : Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+gommit
+======
+
+Git commit on steroids
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/gommit.svg)](https://npmjs.org/package/gommit)
+[![Downloads/week](https://img.shields.io/npm/dw/gommit.svg)](https://npmjs.org/package/gommit)
+[![License](https://img.shields.io/npm/l/gommit.svg)](https://github.com/bcerati/gommit/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g gommit
+$ gommit COMMAND
+running command...
+$ gommit (-v|--version|version)
+gommit/0.0.0 linux-x64 node-v12.14.1
+$ gommit --help [COMMAND]
+USAGE
+  $ gommit COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.js
@@ -0,0 +1,110 @@
+const { execSync } = require('child_process');
+const { Command, flags } = require('@oclif/command');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+/**
+ * Gommit, ask different informations so the commit message can be formatted
+ */
+class GommitCommand extends Command {
+  static flags = {
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+  };
+
+  static strict = false;
+
+  async run() {
+    const { argv } = this.parse(GommitCommand);
+
+    this.log(
+      chalk.green('Please enter the commit informations for your changes.\n')
+    );
+
+    const commitType = await this.askForCommitType();
+    const askForFunctionality = await this.askForFunctionality();
+    const commitMessage = await this.askForCommitMessage();
+    let commitTicketNumber = await this.askForTicketNumber();
+
+    if (commitTicketNumber) {
+      commitTicketNumber = ` (#${commitTicketNumber})`;
+    }
+
+    const finalMessage = `[${askForFunctionality.toUpperCase()}] ${commitType.toLowerCase()} : ${commitMessage}${commitTicketNumber}`;
+
+    execSync(`git commit -m "${finalMessage}" ${argv.join(' ')}`);
+  }
+
+  /**
+   * Ask the type of commit (Hotfix, Feature...)
+   *
+   * @returns {Promise}
+   */
+  async askForCommitType() {
+    return inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'commitType',
+          message: 'What is the type of commit?',
+          choices: ['Hotfix', 'Feature'],
+        },
+      ])
+      .then(({ commitType }) => commitType);
+  }
+
+  /**
+   * Ask the functionnality of commit
+   *
+   * @returns {Promise}
+   */
+  async askForFunctionality() {
+    return inquirer
+      .prompt([
+        {
+          type: 'string',
+          name: 'commitFunctionality',
+          message: 'What is the functionality impacted?',
+        },
+      ])
+      .then(({ commitFunctionality }) => commitFunctionality);
+  }
+
+  /**
+   * Ask the message of commit
+   *
+   * @returns {Promise}
+   */
+  async askForCommitMessage() {
+    return inquirer
+      .prompt([
+        {
+          type: 'string',
+          name: 'commitMessage',
+          message: 'What is the message of your commit?',
+        },
+      ])
+      .then(({ commitMessage }) => commitMessage);
+  }
+
+  /**
+   * Ask for the ticket associated with the modification
+   *
+   * @returns {Promise}
+   */
+  async askForTicketNumber() {
+    return inquirer
+      .prompt([
+        {
+          type: 'string',
+          name: 'commitTicketNumber',
+          message: 'What is the number of your APM ticket?',
+        },
+      ])
+      .then(({ commitTicketNumber }) => commitTicketNumber);
+  }
+}
+
+GommitCommand.description = `Write a Git commit using the convention`;
+
+module.exports = GommitCommand;
